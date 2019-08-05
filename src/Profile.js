354eb0293d6b0b4de0b629114b4bbb2c03387b54
Emerson Lopes <lupuselit@outlook.com>
2019-08-05 20:57:22 -0300

    fetch user info from profile

--- src/Profile.js
+++ src/Profile.js
@@ -12,19 +12,50 @@ const useStyles = makeStyles(theme => ({
   // },
 }));
 
-function Profile() {
+function Profile(props) {
   const classes = useStyles();
 
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+
   useEffect(() => {
-    // localStorage.setItem('name', 'emerson');
-    // localStorage.setItem('isLogged', true);
-  }, []);
+    async function getUserData() {
+      const token = localStorage.getItem('token');
+
+      const response = await fetch('http://localhost:5000/api/user', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, cors, *same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
+
+      const data = await response.json();
+
+      setUserData(data.user);
+      setLoading(false);
+
+      console.log('after fetch ->', loading, userData);
+    }
+    getUserData();
+  }, [loading]);
 
   return (
     <div className={classes.container}>
       <header>
         <p>Ethics eth - you're loged in!</p>
       </header>
+
+      <main>
+        <p>
+          {userData.firstName} {userData.lastName}
+        </p>
+        <p>{userData.email}</p>
+      </main>
 
       <Link to="/register">register</Link>
       <br />
