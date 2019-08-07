bedb0d88c16386bdff6059d95030d39929741480
Emerson Lopes <lupuselit@outlook.com>
2019-08-07 10:16:52 -0300

    f

--- src/Anotations.js
+++ src/Anotations.js
@@ -1,43 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Anotations() {
-
-  const [anotations, setAnotations] = useState({})
+  const [anotations, setAnotations] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function getUserData() {
       const token = localStorage.getItem('token');
 
-      const response = await fetch('http://localhost:5000/api/user/anotations', {
-        method: 'GET', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, cors, *same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
+      const response = await fetch(
+        'http://localhost:5000/api/user/anotations',
+        {
+          method: 'GET',
+          mode: 'cors',
+          cache: 'no-cache',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       const data = await response.json();
 
-      setAnotations(data);
+      setAnotations(data.anotations);
       setLoading(false);
 
-      console.log('after fetch ->', loading, anotations);
+      console.log('anotations ->', loading, anotations);
     }
 
     getUserData();
-    
-  }, [loading])
+  }, [loading]);
+
+  const anotationsComponent = anotations.map(el => (
+    <div key={el._id}>
+      <p> {el.content} </p>
+      <p> {el.createdAt.substring(0,10)} </p>
+    </div>
+  ));
 
   return (
     <div>
       <h1>anotations component!</h1>
+
+      <div>{anotationsComponent}</div>
     </div>
-  )
+  );
 }
 
 export default Anotations;
