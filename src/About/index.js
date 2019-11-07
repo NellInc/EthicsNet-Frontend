0d9a58589f603a975e07231f135015e1683b5c78
Emerson Lopes <lupuselit@outlook.com>
2019-11-07 14:19:17 -0300

    add simple about page

---
+++ src/About/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { useStyles } from './style';
+
+function About() {
+  const classes = useStyles();
+  return (
+    <div className={classes.about}>
+      <div>
+        <p>EthicsNet Annotation Extension – Chrome Version </p>
+        {/* <p>[version/build number string, if that exists]</p> */}
+        <p>Alpha Version 0.0.32 (Release 42)</p>
+        <p>Copyright © 2019, EthicsNet Inc, (11412076), All rights reserved.</p>
+        {/* <p>
+          (later a link to a license, once we figure out what to release it
+          under)
+        </p> */}
+      </div>
+      <p className={classes.p}>
+        Developed by{' '}
+        <a href='http://lupuselit.me' target='_blank' rel='noopener noreferrer'>
+          Emerson Lopes
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default About;
